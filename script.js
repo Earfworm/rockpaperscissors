cb8d1dba7b701199bf68b1f7eaa35b77e16a50c8
earfworm <deshawnreid2021@gmail.com>
2023-12-31 23:07:41 -0500

    going to check for logic issues but basic code down

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-const cpuGameMoves = ["Rock", "Paper", "Scissors"];
-let p1;
-let p2;
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
+const cpuMoves = ["rock", "paper", "scissors"];
+const userInput = prompt("choose: rock, paper, scissors", "rock");
+let p1 = userInput;
+let p2 = cpuMoves[Math.floor(Math.random() * cpuMoves.length)];
+
+const gamePlayRules = () => {
+  if (
+    (p1 === rock && p2 === scissors) ||
+    (p1 === paper && p2 === rock) ||
+    (p1 === scissors && p2 === paper)
+  ) {
+    console.log(`${p1} beats ${p2}, Player One Wins`);
+  } else if (
+    (p2 === rock && p1 === scissors) ||
+    (p2 === paper && p1 === rock) ||
+    (p2 === scissors && p1 === paper)
+  ) {
+    console.log(`${p2} beats ${p1}, Player Two Wins`);
+  } else {
+    console.log(`Draw Game`);
+  }
+};
 
 const startGame = () => {
-  const gameMovesPrompt = prompt("Rock, Paper, Scissors");
   if (
-    gameMovesPrompt !== "Rock" ||
-    gameMovesPrompt !== "Paper" ||
-    gameMovesPrompt !== "Scissors"
+    userInput === rock ||
+    userInput === paper ||
+    (userInput === scissors && typeof userInput === "string")
   ) {
-    console.log("Invalid: Enter Rock, Paper, Scissors");
+    gamePlayRules();
+  } else {
+    console.log("Invalid: Enter rock, paper, scissors");
   }
 };
 
 startGame();
-
-const gamePlayRules = () => {
-  if (
-    (p1 === "Rock" && p2 === "Scissors") ||
-    (p1 === "Paper" && p2 === "Rock") ||
-    (p1 === "Scissor" && p2 === "Paper")
-  ) {
-    console.log(`${p1} beats ${p2}, Player One Wins`);
-  } else {
-    console.log(`${p2} beats ${p1}, Player One Wins`);
-  }
-};
