3a808758d49430747d7e8e4cdb47642012242f6c
earfworm <deshawnreid2021@gmail.com>
2024-01-01 19:47:15 -0500

    start btn addeventlistener

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
+const startBtn = document.querySelector("button");
 const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
 const cpuMoves = ["rock", "paper", "scissors"];
-const userInput = prompt("choose: rock, paper, scissors", "rock").toLowerCase();
-let p1 = userInput;
+
 let p2 = cpuMoves[Math.floor(Math.random() * cpuMoves.length)];
 
-const gamePlayRules = () => {
-  if (
-    (p1 === rock && p2 === scissors) ||
-    (p1 === paper && p2 === rock) ||
-    (p1 === scissors && p2 === paper)
-  ) {
-    console.log(`${p1} beats ${p2}, Player One Wins`);
-  } else if (
-    (p2 === rock && p1 === scissors) ||
-    (p2 === paper && p1 === rock) ||
-    (p2 === scissors && p1 === paper)
-  ) {
-    console.log(`${p2} beats ${p1}, Player Two Wins`);
-  } else {
-    console.log(`Draw Game`);
-  }
-};
-
 const startGame = () => {
+  const userInput = prompt(
+    "choose: rock, paper, scissors",
+    "rock"
+  ).toLowerCase();
+  let p1 = userInput;
+  const gamePlayRules = () => {
+    if (
+      (p1 === rock && p2 === scissors) ||
+      (p1 === paper && p2 === rock) ||
+      (p1 === scissors && p2 === paper)
+    ) {
+      console.log(`${p1} beats ${p2}, Player One Wins`);
+    } else if (
+      (p2 === rock && p1 === scissors) ||
+      (p2 === paper && p1 === rock) ||
+      (p2 === scissors && p1 === paper)
+    ) {
+      console.log(`${p2} beats ${p1}, Player Two Wins`);
+    } else {
+      console.log(`Draw Game`);
+    }
+  };
   if (
     userInput === rock ||
     userInput === paper ||
@@ -36,4 +40,4 @@ const startGame = () => {
   }
 };
 
-startGame();
+startBtn.addEventListener("click", startGame);
